2a00218800339496740940a6006cf9e752617505
Suliaman Biswas <sulaimanbiswasbd@gmail.com>
2022-10-01 04:53:27 +0600

    --- Auto Git Commit ---

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -13,8 +13,47 @@ const Activities = () => {
         </div>
       </div>
       <div className="user-details">
-        
+        <div className="weight">
+          <h2>
+            76<span>kg</span>
+          </h2>
+          <p>Weight</p>
+        </div>
+        <div className="height">
+          <h2>6.5</h2>
+          <p>Height</p>
+        </div>
+        <div className="weight">
+          <h2>
+            25<span>yrs</span>
+          </h2>
+          <p>Age</p>
+        </div>
       </div>
+
+      <div className="user-profile">
+        <h2>Add A Break</h2>
+      </div>
+      <div className="user-details">
+        <button>10s</button>
+        <button>20s</button>
+        <button>30s</button>
+        <button>40s</button>
+        <button>50s</button>
+      </div>
+
+      <div className="user-profile">
+        <h2>Exercise Details</h2>
+      </div>
+      <div className="user-details">
+        <h2>Exercise time</h2>
+        <p>10 seconds</p>
+      </div>
+      <div className="user-details">
+        <h2>Break time</h2>
+        <p>10 seconds</p>
+      </div>
+      <button className="add-to-list">Activity Completed</button>
     </div>
   );
 };
